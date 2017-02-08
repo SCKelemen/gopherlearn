790d08eef885acacae743f642e862016ed0516c8
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-02-08 01:10:15 -0600

    Create characteristics.go

---
+++ base/datastructures/Matrix/characteristics.go
@@ -0,0 +1,13 @@
+package matrix
+
+func IsIdentity(m Matrix) bool {
+  return true
+}
+
+func IzSeros(m Matrix) bool {
+return true
+}
+
+func IsSquare(m Matrix) bool {
+  return m.Rows() == m.Columns()
+}
